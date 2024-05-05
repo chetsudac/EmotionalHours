6a8676e8d266fcb1a336d60aeedee0a8e1322e5f
chetsudac <chalc313@newschool.edu>
2024-05-05 18:05:26 -0400

    -

--- tracklist.js
+++ tracklist.js
@@ -6,18 +6,9 @@ options = {
   clock()
   setInterval(clock, 1000);
 
-document.body.style.background = "linear-gradient(to bottom, pink, navy)"
 const body = document.querySelector('body');
 const light = document.querySelector('light');
 const date = new Date();
 const hour = date.getHours();
-
-if (hour < 12) {
-  body.style.backgroundColor = 'lightblue';
-} else if (hour < 16) {
-  body.style.background = "linear-gradient(to bottom, #7890E4, #E78840)";
-} else {
-  body.style.background = "linear-gradient(to bottom, #11114D, #FFAD62)";
-}
 
 
